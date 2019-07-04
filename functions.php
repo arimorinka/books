19049451d0f9415c964169302456e86b53240a12
soma <moradi68soma@gmail.com>
2019-07-04 17:05:50 +0430

    Save Metabox

--- functions.php
+++ functions.php
@@ -172,3 +172,47 @@ function wpt_books_isbn() {
     echo '<input type="text" name="isbn" value="' . esc_textarea( $isbn )  . '" class="widefat">';
 }
 
+
+/**
+ * Save the metabox data
+ */
+function wpt_save_books_meta( $post_id, $post ) {
+    // Return if the user doesn't have edit permissions.
+    if ( ! current_user_can( 'edit_post', $post_id ) ) {
+        return $post_id;
+    }
+
+    // Verify this came from the our screen and with proper authorization,
+    // because save_post can be triggered at other times.
+    if ( ! isset( $_POST['isbn'] ) || ! wp_verify_nonce( $_POST['book_fields'], basename(__FILE__) ) ) {
+        return $post_id;
+    }
+    // Now that we're authenticated, time to save the data.
+    // This sanitizes the data from the field and saves it into an array $books_meta.
+    $books_meta['isbn'] = esc_textarea( $_POST['isbn'] );
+    // Cycle through the $books_meta array.
+    // Note, in this example we just have one item, but this is helpful if you have multiple.
+    foreach ( $books_meta as $key => $value ) :
+        // Don't store custom data twice
+        if ( 'revision' === $post->post_type ) {
+            return;
+        }
+        if ( get_post_meta( $post_id, $key, false ) ) {
+            // If the custom field already has a value, update it.
+
+            update_post_meta( $post_id, $key, $value );
+
+        } else {
+            // If the custom field doesn't have a value, add it.
+            add_post_meta( $post_id, $key, $value);
+
+        }
+        if ( ! $value ) {
+            // Delete the meta key if there's no value
+            delete_post_meta( $post_id, $key );
+        }
+    endforeach;
+
+}
+add_action( 'save_post', 'wpt_save_books_meta', 1, 2 );
+
